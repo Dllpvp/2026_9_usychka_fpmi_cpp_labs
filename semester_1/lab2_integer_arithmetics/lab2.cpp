931f39788e4a7b19ca65e54d73d2fdc6a3236557
dllpvp <MyNet33.bus@gmail.com>
2026-09-15 22:18:50 +0300

    Add files via upload
    Вторая лаба, 8 вар

---
+++ semester_1/lab2_integer_arithmetics/lab2.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::swap;
+
+
+
+
+void easy(int c) {
+
+	for (int i = 2; i <= sqrt(c); i++) // число простое с 2 (а по факту с 3).
+		if (c % i == 0) {
+			cout << "[38;2;160;160;160m" << c << " [0m"; //серым обозначим сложные числа
+			return;
+		}
+	std::cout << "[38;2;50;205;50m" << c << " [0m"; //зеленым - искомые числа
+}
+
+
+void z8() {
+	double a, b;
+	int ai, bi;//a_integer;b_integer
+
+	cout << "Введите a:\n";
+	cin >> a;
+	cout << "Введите b:\n";
+	cin >> b;
+	if (a > b) 
+		swap(a, b); //Нормальный промежуток: b>=a
+	ai = a;
+	bi = b;
+	for (int i = a; i <= b; i++) {
+		easy(i); //перебор всех чисел промежуток (интеджеров)
+	}
+}
+
+
+int main()
+{
+	setlocale(0, "");
+	z8();
+
+	return 0;
+}
